f8a8922401854431d3c0eb2e0d7539ddb95eff01
Tracy-miranja <barazatracy44@gmail.com>
2023-03-09 11:03:05 +0300

    add delete file to my mocks folder

--- __mocks__/add.test.js
+++ __mocks__/add.test.js
@@ -56,7 +56,6 @@ describe('addTask function', () => {
   });
 
   test('should add a new task to the list', () => {
-    // Arrange
     const description = 'Task 1';
     const container = document.createElement('div');
     container.innerHTML = `
@@ -73,50 +72,12 @@ describe('addTask function', () => {
     const form = container.querySelector('form');
     const input = container.querySelector('#item');
 
-    // Act
     if (input) {
       input.value = description;
       form.dispatchEvent(new Event('submit'));
     }
 
-    // Assert
     expect(container.querySelectorAll('#task-list li').length).not.toBe(0);
     expect(container.querySelector('#task-list li:last-child').textContent).toBe(description);
-  });
-});
-
-describe('deleteTask function', () => {
-  it('should delete a task from the items array', () => {
-    const items = [
-      { description: 'Task 1', completed: false, index: 0 },
-      { description: 'Task 2', completed: false, index: 1 },
-      { description: 'Task 3', completed: false, index: 2 },
-    ];
-    const indexToDelete = 1;
-    const expectedItems = [
-      { description: 'Task 1', completed: false, index: 0 },
-      { description: 'Task 3', completed: false, index: 1 },
-    ];
-
-    const result = deleteTask(items, indexToDelete);
-
-    expect(result).toEqual(expectedItems);
-  });
-
-  it('should update the index property of the remaining tasks', () => {
-    const items = [
-      { description: 'Task 1', completed: false, index: 0 },
-      { description: 'Task 2', completed: false, index: 1 },
-      { description: 'Task 3', completed: false, index: 2 },
-    ];
-    const indexToDelete = 1;
-    const expectedItems = [
-      { description: 'Task 1', completed: false, index: 0 },
-      { description: 'Task 3', completed: false, index: 1 },
-    ];
-
-    const result = deleteTask(items, indexToDelete);
-
-    expect(result).toEqual(expectedItems);
   });
 });
